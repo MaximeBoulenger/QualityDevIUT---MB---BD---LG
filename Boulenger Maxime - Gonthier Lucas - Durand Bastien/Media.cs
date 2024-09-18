87ed6f7bd7b798ed4448fabbc07b31abccd2582f
LeyKoz <lucas.gonthier@etud.u-picardie.fr>
2024-09-18 18:58:06 +0200

    création des class Media, Livre, DVD, CD

---
+++ Boulenger Maxime - Gonthier Lucas - Durand Bastien/Media.cs
@@ -0,0 +1,10 @@
+﻿namespace DefaultNamespace;
+
+public class Media
+{
+    private string Title { get; set; }
+    
+    private int NumRef { get; set; }
+    
+    private int NbAvailable { get; set; }
+}
