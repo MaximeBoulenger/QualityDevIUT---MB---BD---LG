77d3d967d8632cdea6992511784383d31e434664
Bastien DURAND <thekilleur.omax2016@gmail.com>
2024-09-18 19:38:54 +0200

    Exercice 2

--- Boulenger Maxime - Gonthier Lucas - Durand Bastien/Media.cs
+++ Boulenger Maxime - Gonthier Lucas - Durand Bastien/Media.cs
@@ -2,7 +2,18 @@
 
 public class Media
 {
-    private string Title { get; set; }
+    public string Title { get; private set; }
+    
+    public int NumRef { get; private set; }
+    
+    public int NbAvailable { get; private set; }
+    
+    public Media(string title, int numRef, int nbAvailable)
+    {
+        Title = title;
+        NumRef = numRef;
+        NbAvailable = nbAvailable;
+    }
     
     private int NumRef { get; set; }
     
@@ -12,4 +23,22 @@ public class Media
         {
             Console.WriteLine($"Title: {Title}, NumRef: {NumRef}, NbAvailable: {NbAvailable}");
         }
+    //Surcharger opérateur + 
+    public static Media operator +(Media p_media, int p_quantity)
+    {
+        p_media.NbAvailable += p_quantity;
+        return p_media;
+    }
+
+    //Surcharge opérateur - 
+    public static Media operator -(Media p_media,int p_quantity)
+    {
+        if (p_media.NbAvailable < p_quantity)
+        {
+            throw new InvalidOperationException("Quantité à retirer dépasse le nombre disponible.");
+        }
+        
+        p_media.NbAvailable -= p_quantity;
+        return p_media;
+    }
 }
